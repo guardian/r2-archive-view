2f6adc6f2d293f7ebc732dd663579cf999350943
Thomas McKiernan <thomas.mckiernan.freelance@guardian.co.uk>
2016-02-02 09:48:34 +0000

    some more structure

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api._
+import play.api.db._
 import play.api.mvc._
 import services.aws.AwsInstanceTags
 
@@ -12,9 +13,28 @@ object Application extends Controller with AwsInstanceTags{
     Ok(views.html.index(stage))
   }
 
+  private def getDbHealth : Boolean = {
+    DB.withConnection { conn =>
+      // do whatever you need with the connection
+      val conn = DB.getConnection()
+      try {
+        val stmt = conn.createStatement
+        val rs = stmt.executeQuery("SELECT * from Migrated_Content where rownum=1")
+        if (rs.next()) true
+        else false
+      } finally {
+        conn.close()
+      }
+    }
+  }
+
 
   def healthcheck = Action {
-    Ok("healthcheck is OK")
+    val healthCheck : List[Boolean] = getDbHealth :: Nil
+    healthCheck.foldLeft(true)(_ && _) match {
+      case true => Ok("healthcheck is OK")
+      case false => InternalServerError("healthcheck failed")
+    }
   }
 
 }
--- project/R2ArchiveViewApp.scala
+++ project/R2ArchiveViewApp.scala
@@ -4,13 +4,14 @@ import play.PlayImport._
 import sbt._
 import sbt.Keys._
 
-object GdnAwsPlaySampleApp extends Build {    //TODO: replaceme name of this file should match your project
+object R2ArchiveViewApp extends Build {
 
 
   val libDependencies = Seq(
     "com.amazonaws" % "aws-java-sdk" % "1.9.23",
     "net.logstash.logback" % "logstash-logback-encoder" % "4.2")
 
+  libraryDependencies += jdbc
 
   val commonSettings = Seq(
     scalaVersion := "2.11.1",
@@ -22,10 +23,10 @@ object GdnAwsPlaySampleApp extends Build {    //TODO: replaceme name of this fil
     doc in Compile <<= target.map(_ / "none"),
     incOptions := incOptions.value.withNameHashing(nameHashing = true)
   )
-  val projectName = "gdn-aws-play-sample-app" //TODO: replaceme
+  val projectName = "r2-archive-view-app"
   lazy val root = Project(projectName, file(".")).enablePlugins(play.PlayScala)
     .settings(commonSettings ++ playArtifactDistSettings :_*)
-    .settings(libraryDependencies ++= libDependencies)
+    .settings(libraryDependencies  ++= libDependencies)
     .settings(magentaPackageName := projectName)
-    .settings(playDefaultPort := 9100)    //TODO: try to make this unique to avoid local machine clash
+    .settings(playDefaultPort := 9301)
 }

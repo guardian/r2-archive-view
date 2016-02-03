9666c8b25580800085bd99950a77a24732594cb6
Thomas McKiernan <thomas.mckiernan.freelance@guardian.co.uk>
2016-02-03 16:31:22 +0000

    support for riffraff

--- project/R2ArchiveViewApp.scala
+++ project/R2ArchiveViewApp.scala
@@ -3,6 +3,9 @@ import play.PlayImport.PlayKeys._
 import play.PlayImport._
 import sbt._
 import sbt.Keys._
+import com.gu.riffraff.artifact._
+import RiffRaffArtifact.autoImport._
+import com.typesafe.sbt.packager.Keys._
 
 object R2ArchiveViewApp extends Build {
 
@@ -15,8 +18,8 @@ object R2ArchiveViewApp extends Build {
 
 
   val commonSettings = Seq(
-    scalaVersion := "2.11.1",
-    scalaVersion in ThisBuild := "2.11.1",
+    scalaVersion := "2.11.7",
+    scalaVersion in ThisBuild := "2.11.7",
     organization := "com.gu",
     version      := "0.1",
     resolvers ++= Seq("Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"),
@@ -25,9 +28,23 @@ object R2ArchiveViewApp extends Build {
     incOptions := incOptions.value.withNameHashing(nameHashing = true)
   )
   val projectName = "r2-archive-view"
-  lazy val root = Project(projectName, file(".")).enablePlugins(play.PlayScala)
-    .settings(commonSettings ++ playArtifactDistSettings :_*)
-    .settings(libraryDependencies  ++= libDependencies)
-    .settings(magentaPackageName := projectName)
-    .settings(playDefaultPort := 9301)
+  lazy val root =
+    Project(projectName, file("."))
+      .enablePlugins(play.PlayScala)
+      .enablePlugins(RiffRaffArtifact)
+      .settings(commonSettings ++ playArtifactDistSettings :_*)
+      .settings(libraryDependencies  ++= libDependencies)
+      .settings(magentaPackageName := projectName)
+      .settings(playDefaultPort := 9301)
+      .settings(
+        riffRaffPackageType := (packageZipTarball in config("universal")).value,
+        riffRaffPackageName := name.value,
+        riffRaffManifestProjectName := s"r2ArchiveView:${name.value}",
+        riffRaffBuildIdentifier := Option(System.getenv("CIRCLE_BUILD_NUM")).getOrElse("DEV"),
+        riffRaffUploadArtifactBucket := Option("riffraff-artifact"),
+        riffRaffUploadManifestBucket := Option("riffraff-builds")
+      )
+
+
+
 }
